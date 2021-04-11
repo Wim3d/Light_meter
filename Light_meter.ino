60813651805dac8ece9a80a03e2a87585679a2ab
Wim3d <wim3d@casema.nl>
2021-04-11 11:53:15 +0200

    Improved display of large and small values
    Values > 9.999 fit nicely
    Below 100 1 decimal is shown
    Below 10 2 decimals are shown

--- Light_meter.ino
+++ Light_meter.ino
@@ -1,7 +1,8 @@
 /*
-  Light meter using a BH1750 Lux sensor
+  Lux sensor
   W.J. Hoogervorst april 2021
   Based on example of BH1750 library.
+
 */
 
 #include <U8x8lib.h>
@@ -18,11 +19,11 @@ void setup() {
   u8x8.begin();
   u8x8.setPowerSave(0);
   u8x8.setFont(u8x8_font_inb21_2x4_f);
-    // Initialize the I2C bus (BH1750 library doesn't do this automatically)
+  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
   Wire.begin();
 
   lightMeter.begin();
-  
+
   //measure battery voltage
   // 3.6 - 4.2 is 0 % - 100 %
   float voltage;
@@ -52,25 +53,42 @@ void setup() {
 
 
 void loop() {
-  int lux = lightMeter.readLightLevel();
+  u8x8.clear();
+  float lux = lightMeter.readLightLevel();
   Serial.print("Light: ");
   Serial.print(lux);
   Serial.println(" lx");
-
-  char buf2[20];
-  sprintf(buf2, "%d", lux);
-
-  char buf3[20] = " Lx";
-  strcat (buf2, buf3);
-
-  u8x8.clear();
+  if (lux < 10)
+  {
+    String lux_s = String(lux);
+    lux_s += " Lx";
+    char buf[10];
+    lux_s.toCharArray(buf, lux_s.length() + 1);
+    u8x8.drawString(2 * (1 - (int)log10(lux)), 0, buf);
+  }
+  else if (lux < 100)
+  {
+    String lux_s = String(lux);
+    String short_lux_s = lux_s.substring(0, lux_s.length() - 1);
+    short_lux_s += " Lx";
+    char buf[10];
+    short_lux_s.toCharArray(buf, short_lux_s.length() + 1);
+    u8x8.drawString(2 * (2 - (int)log10(lux)), 0, buf);
+  }
+  else {
+    uint32_t lux_rounded0 = lux * (int)100 / 100;
+    char buf2[20];
+    sprintf(buf2, "%d", lux_rounded0);
+    char buf3[20] = " Lx";
+    strcat (buf2, buf3);
+    u8x8.drawString(2 * (4 - (int)log10(lux)), 0, buf2);
+  }
+  delay(1000);
   // The screen is 32 x 128 pixels, in the 8*8 library this results in 32/4 = 4 lines (0 - 3) and 128/8 = 16 columns (0 - 15).
   // The font uses 2*4 squares of 8*8 pixels. This results in 1 line (32/4/8 = 1) and 8 characters (128/2/8 = 8)
-  // The 8 characters are sufficient for showing the maximum value of "65535_Lx".  
+  // The 8 characters are sufficient for showing the maximum value of "65535_Lx".
   // The string is positioned at the right of the screen. The indent depends on the amount of figures in the value
-  // The number of figures is derrived from the 10Log of the value (0 - 65535), which can have 1 - 5 figures.
-  // The font has 2 positions for one character, that's why the outcome is multiplied by 2. 
-  // A value of 5 figures starts at column 0 (most left), a value of 2 figures starts at column 3*2 = column 6 
-  u8x8.drawString(2 * (5 - (int)log10(lux)), 0, buf2); 
-  delay(1000);
+  // The number of figures is derrived from the 10Log of the value (0 - 65535), which can have 1 - 5 figures, 10Log is 0 - 4.
+  // The font has 2 positions for one character, that's why the outcome is multiplied by 2.
+  // A value of 5 figures starts at column 0 (most left), a value of 2 figures starts at column 3*2 = column 6
 }
